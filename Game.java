03e11c91c8ae147ce9fbcb70ada4893ea762ef48
JoeMcLaughlin <jomclis@gmail.com>
2015-11-30 00:55:31 -0800

    Update Sunday November 29th-Joe
    Adds theModel to the game class.
    Adds the printRules() method to the main

--- Game.java
+++ Game.java
@@ -3,8 +3,12 @@ package ad310_Group;
 public class Game {
 	public static void main(String[] args){
 		ViewControllerBattleShip theViewController = new ViewControllerBattleShip();
-	
+		BattleShipModel theModel = new BattleShipModel();
+		
+		
+		theViewController.printRules();
 		theViewController.displayPlayer1Placement();
 		theViewController.setUp();
+		
 	}
 }
